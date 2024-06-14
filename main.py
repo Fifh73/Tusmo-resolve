2fd0602b28051693fa8d836b176568ffce8948ab
Fifh73 <fantin.mergnac@hotmail.fr>
2024-06-14 16:10:28 +0200

    Code de base du resolveur : connexion sur une partie, détecte la première lettre du mot, compte le nombre de lettres du mot

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Bonjour2")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+#Fonction qui compte le nombre de lettres du mot
+def nb_case():
+    all_case = driver.find_elements(By.CSS_SELECTOR, ".cell-content")
+    l_mot = len(all_case) / 6
+    return int(l_mot)
+
+#Instance chrome
+driver = webdriver.Chrome()
+
+#Ouvre le site web
+driver.get("https://www.tusmo.xyz/sb88ac8f")
+
+#Récupere la valeure de la lettre de départ
+element = driver.find_element(By.CSS_SELECTOR, ".cell-content")
+print(element.text)
+
+#Récupère le nombre de lettre du mot
+longueur_mot = nb_case()
+print("nombre de lettres :", longueur_mot)
+
+#Ferme l'instance chrome
+#time.sleep(3)
+driver.quit()
