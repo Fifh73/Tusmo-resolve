3b8d74991a0699e360c7fd1daa2b9811fe025e4d
Fifh73 <fantin.mergnac@hotmail.fr>
2024-06-17 10:40:58 +0200

    Tri des mots en fonction de leur nombre de lettres et de la première lettre du mot. Ajout de la librairie pg permettant d'écrire dans l'interface de selenium

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import pyautogui as pg
+import json
 import time
 
 #Fonction qui compte le nombre de lettres du mot
@@ -12,16 +14,33 @@ def nb_case():
 driver = webdriver.Chrome()
 
 #Ouvre le site web
-driver.get("https://www.tusmo.xyz/sb88ac8f")
+driver.get("https://www.tusmo.xyz/s411f0c5")
 
 #Récupere la valeure de la lettre de départ
-element = driver.find_element(By.CSS_SELECTOR, ".cell-content")
-print(element.text)
+lettre = driver.find_element(By.CSS_SELECTOR, ".cell-content")
+l1 = lettre.text
 
 #Récupère le nombre de lettre du mot
 longueur_mot = nb_case()
 print("nombre de lettres :", longueur_mot)
 
+#Trie la liste de mots en fontcion du nombre de lettres et les enregistre dans la liste liste_long
+liste_long = []
+with open('./mots-stat.json', 'r') as f:
+    data = json.load(f)
+for mot in data:
+    if len(mot) == longueur_mot:
+        liste_long.append(mot)
+
+#Trie la liste de mot en fonction de la premiere lettre
+lettre_liste = []
+for t_mot in liste_long:
+    if t_mot[0] == l1.lower():
+        lettre_liste.append(t_mot)
+
+pg.write(lettre_liste[1])
+pg.press('enter')
+
 #Ferme l'instance chrome
-#time.sleep(3)
+time.sleep(3)
 driver.quit()
